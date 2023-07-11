a213d1b7e17704d9a462154bcc6a55c06edd09cf
Aadarsh-Keshri <120744385+Aadarsh-Keshri@users.noreply.github.com>
2023-07-11 21:34:27 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const express=require('express');
 const {ServerConfig}=require('./config');   
 const apiRoutes = require('./routes');
 
-const mailsender=require('./config/email-config');
 const app = express();
 
 app.use(express.json());
@@ -19,15 +18,4 @@ app.use('/api',apiRoutes);
 
 app.listen(ServerConfig.PORT,async ()=>{
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try {
-        const response=await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'aadarshcodes@gmail.com',
-            subject: 'Is the service working?',
-            text: 'Haawww Yeahhhhh!!!!'
-        });
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
 });
