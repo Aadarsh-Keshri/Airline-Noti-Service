f1b4dc1de6447189f987adb098564be0d8a1638a
Aadarsh-Keshri <120744385+Aadarsh-Keshri@users.noreply.github.com>
2023-07-12 21:31:26 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-/**
- * to run "dev" in script of package.json, $'use npm run dev'
- */
-
-
 const express=require('express');
+const amqplib=require('amqplib');
+const {EmailService}=require('./services');
+const {ServerConfig}=require('./config')
 
-const {ServerConfig}=require('./config');   
+async function connectQueue(){
+    try {
+        const connection=await amqplib.connect("amqp://localhost");
+        const channel=await connection.createChannel();
+
+        await channel.assertQueue("noti-queue");
+        channel.consume('noti-queue',async (data)=>{
+            console.log(`${Buffer.from(data.content)}`);
+            const object=JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail(ServerConfig.GMAIL_EMAIL,object.recepientEmail,object.subject,object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -18,4 +32,5 @@ app.use('/api',apiRoutes);
 
 app.listen(ServerConfig.PORT,async ()=>{
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    connectQueue();
 });
